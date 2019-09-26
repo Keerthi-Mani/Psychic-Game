bf1a887d1ebd76343d6ca1bb70f1cb30d6e5bdc9
Keerthi Mani <carkeane.getz@gmail.com>
2019-09-26 19:03:31 -0400

    Javascript is used in Guessing Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,61 @@
+//create an array
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//creating variables
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessChoices = [];
+
+//create variables that hold references to the places in the HTML where we want to display things.
+
+var userChoiceText = document.getElementById("userchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var remaining = document.getElementById("guessesleft-text");
+var soFarGuessed = document.getElementById("guessedsofar-text");
+
+//function runs whenever the user presses the key
+document.onkeyup = function (event) {
+
+    //determines which key was pressed
+    var userGuess = event.key.toLowerCase();
+
+    //alert the user to type only the alphabet keys
+    if (event.which < 65 || event.which > 90) {
+        alert("Only a to z characters are allowed");
+        return;
+    }
+
+    //Randomly chooses a choice from the options array from [0 to 25]. This is a computer's guess
+
+
+    if (userGuess === computerGuess) {
+        wins++;
+        guessesLeft = 9;
+        guessChoices = [];
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    }
+    if (userGuess !== computerGuess) {
+        guessesLeft--;
+        guessChoices.push(userGuess);
+
+    }
+    if (guessesLeft === 0) {
+        losses++;
+        alert("GAME OVER!!");
+        guessesLeft = 9;
+        guessChoices = [];
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    }
+
+
+    //Displays in the screen
+    userChoiceText.textContent = "You chose : " + userGuess;
+    winsText.textContent = "Wins : " + wins;
+    lossesText.textContent = "Losses : " + losses;
+    remaining.textContent = "Guesses Left : " + guessesLeft;
+    soFarGuessed.textContent = "Your Guesses so far : " + guessChoices;
+
+};
